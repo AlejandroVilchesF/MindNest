7df61cdc9897a4e50b167f819e827c37fb72b3d1
alejandro <alejandro.vilches.fernandez@gmail.com>
2025-07-11 12:10:44 +0200

    feat(board): note component style controlled by the completed status attribute

--- 1.frontend/src/components/UI/NoteItem.jsx
+++ 1.frontend/src/components/UI/NoteItem.jsx
@@ -1,7 +1,12 @@
 function NoteItem({ note, onClick }) {
+
   if (!note.parent_note) {
     return <h1 className="font-bold text-lg mb-2" key={note.id}>{note.note_title}</h1>;
   }
-  return <p className="bg-white hover:bg-gray-100 rounded-md p-2 mb-2 shadow-sm text-sm text-gray-700 cursor-pointer" key={note.id} onClick={() => onClick(note)}>{note.note_title}</p>;
+  
+  const completionBgClass = note.completed ? "bg-green-100 hover:bg-green-50": "bg-white hover:bg-gray-100"
+
+  return <p className={`${completionBgClass} rounded-md p-2 mb-2 shadow-sm text-sm text-gray-700 cursor-pointer`} key={note.id} onClick={() => onClick(note)}>{note.note_title}</p>;
 }
+
 export default NoteItem;
